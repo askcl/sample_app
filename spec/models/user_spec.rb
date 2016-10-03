0c3ff0bcbb57ed824e8e06d28b814985eb0fdb34
Askcl <askcl@mail.ru>
2016-10-03 13:05:40 +0300

    till 11.1.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
 
   it { should be_valid }
 
